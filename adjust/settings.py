762cbd1cbee6bfe9721286007ec90f0dc7892555
Jayden Shamhu <jayden@siege.ai>
2019-07-09 12:12:25 +0200

    added database settings, removed cached .idea directory

--- adjust/settings.py
+++ adjust/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'adjust.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adjust',
+        'USER': 'adjust',
+        'PASSWORD': 'adjust',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 600,
     }
 }
 
